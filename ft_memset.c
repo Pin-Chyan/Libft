094e0f0f7c9f13472fdde24ef3eb3a949b98e17b
Event WeThinkCode <event@c3r8s7.wethinkcode.co.za>
2019-05-19 13:22:45 +0200

    update functions 19 may

--- ft_memset.c
+++ ft_memset.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/18 14:58:14 by event             #+#    #+#             */
-/*   Updated: 2019/05/19 09:41:53 by event            ###   ########.fr       */
+/*   Created: 2019/05/19 09:57:22 by event             #+#    #+#             */
+/*   Updated: 2019/05/19 10:36:59 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include <libft.c>
 
-int		ft_isascii(int c)
+void	memset(void *b, int c, size_t len)
 {
-	return ( c >= 0 && c <= 127)
-}
+	char	*ptr;
+	size_t	i;
 
+	str = b;
+	i = 0;
+	while (i < len)
+		*(ptr + i++) = c;
+	return(b);
+}
