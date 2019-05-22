8cd9d3a1aec49171cdc6400110715c98fb83bf2b
Pin-Chyan Tsai <pitsai@c3r6s6.wethinkcode.co.za>
2019-05-22 10:49:15 +0200

    strcat & strncat update

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pitsai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 14:50:14 by pitsai            #+#    #+#             */
-/*   Updated: 2019/05/22 10:19:11 by pitsai           ###   ########.fr       */
+/*   Created: 2019/05/22 10:22:05 by pitsai            #+#    #+#             */
+/*   Updated: 2019/05/22 10:48:46 by pitsai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char	*strcat(char *restrict s1, const char *restrict s2)
+char	strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
 	int		i;
 	int		j;
 
 	i = -1;
-	j = (int)(ft_strlen(s1);
-			while (*()s2 + ++i))
-		*(s1 +j++) = *(s2 + i);
+	j = (int)ft_strlen(s1);
+
+	while (*(s2 ++i) && i < (int)n)
+		*(s1 + j++) = *(s2 + i);
 	*(s1 + j) = '\0';
 	return (s1);
 }
