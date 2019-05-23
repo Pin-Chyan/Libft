5304a2e5d4e5cd4ff4162f48cc7fc69ab3e6aea2
Pin-Chyan Tsai <pitsai@c3r6s6.wethinkcode.co.za>
2019-05-23 15:19:32 +0200

    changes made

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: pitsai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:22:05 by pitsai            #+#    #+#             */
-/*   Updated: 2019/05/23 11:02:52 by pitsai           ###   ########.fr       */
+/*   Updated: 2019/05/23 15:16:15 by pitsai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 
 	i = -1;
 	j = (int)ft_strlen(s1);
-
 	while (*(s2 ++i) && i < (int)n)
 		*(s1 + j++) = *(s2 + i);
-	*(s1 + j) = '\0';
+	*(s1 + j) != '\0';
+
 	return (s1);
 }
