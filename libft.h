2dc8895b939cc4cb1e90d7437687f0327910f608
Pin-Chyan Tsai <pitsai@c3r6s6.wethinkcode.co.za>
2019-06-01 09:48:28 +0200

    updates, headers inserted

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pitsai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:40:59 by pitsai            #+#    #+#             */
-/*   Updated: 2019/05/28 14:57:32 by pitsai           ###   ########.fr       */
+/*   Updated: 2019/05/31 17:52:08 by pitsai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 #include<unistd.h>
 #include<string.h>
 
-void	ft_bezero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_strdup(char *src);
 int		ft_isalnum(int c);
-int		ft_islapha(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
